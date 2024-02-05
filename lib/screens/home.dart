1af4a84489a17902a6174b6587279cb9eaa13949
dshreddy03 <112101014@smail.iitpkd.ac.in>
2024-02-05 11:13:15 +0530

    alinged content in home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -32,29 +32,47 @@ class _HomeScreenState extends State<HomeScreen> {
             SizedBox(
               height: 20,
             ),
-            Text(
-              "\u2022 Course feedback section, here we can find the feed back of the people who took the course in the past.",
-              style: TextStyle(
-                fontSize: 20,
-              ),
+            Row(
+              children: <Widget>[
+                Flexible(
+                  child: Text(
+                    "\u2022 Course feedback section, here we can find the feed back of the people who took the course in the past.",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
             ),
             SizedBox(
               height: 20,
             ),
-            Text(
-              "\u2022 News, here we can find the links to the news articles published in new papers about our institute community & also links to any of their research publications.",
-              style: TextStyle(
-                fontSize: 20,
-              ),
+            Row(
+              children: <Widget>[
+                Flexible(
+                  child: Text(
+                    "\u2022 News, here we can find the links to the news articles published in new papers about our institute community & also links to any of their research publications.",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
             ),
             SizedBox(
               height: 20,
             ),
-            Text(
-              "\u2022 Off campus job updates, here we can get notifications about off campus opportunities.",
-              style: TextStyle(
-                fontSize: 20,
-              ),
+            Row(
+              children: <Widget>[
+                Flexible(
+                  child: Text(
+                    "\u2022 Off campus job updates, here we can get notifications about off campus opportunities.",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
             ),
             Expanded(
               child: SizedBox(),
