764b8b33ef989ffea7819c0832bb94c4687a1e48
dshreddy03 <112101014@smail.iitpkd.ac.in>
2024-02-25 15:57:50 +0530

    v1

--- lib/screens/about_me.dart
+++ lib/screens/about_me.dart
@@ -16,28 +16,35 @@ class _AboutMeState extends State<AboutMe> {
         child: Column(
           children: [
             Expanded(child: const SizedBox()),
-            const Text(
+            Text(
+              "About the developer :",
+              style: TextStyle(
+                fontSize: 10,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Text(
               "D. Sai Hemanth Reddy",
               style: TextStyle(
                 fontSize: 30,
                 fontWeight: FontWeight.w700,
               ),
             ),
-            const Text(
+            Text(
               "B.Tech CSE",
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.w700,
               ),
             ),
-            const Text(
+            Text(
               "IIT Palakkad",
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.w700,
               ),
             ),
-            Expanded(child: const SizedBox()),
+            Expanded(child: SizedBox()),
           ],
         ),
       ),
