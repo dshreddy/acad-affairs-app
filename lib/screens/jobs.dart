485d38cf3f8c9376e36f942df47ed26a10c4816f
dshreddy03 <112101014@smail.iitpkd.ac.in>
2024-03-07 19:05:24 +0530

    updated jobs

--- lib/screens/jobs.dart
+++ lib/screens/jobs.dart
@@ -15,14 +15,6 @@ class _JobsState extends State<Jobs> {
   // Sample job listings data
   final List<JobListing> jobListings = [
     JobListing(
-      company: 'Google',
-      batch: 'PHD',
-      role: 'Software Developer Intern',
-      salary: 'Not specified',
-      link:
-          'https://www.google.com/about/careers/applications/jobs/results/112996546481398470-software-developer-intern-phd-summer-2024?target_level=INTERN_AND_APPRENTICE',
-    ),
-    JobListing(
       company: 'Goldman Sachs',
       batch: 'UG + PG in 2nd year / pre final year',
       role: '2024 Summer Analyst Program',
@@ -30,6 +22,27 @@ class _JobsState extends State<Jobs> {
       link:
           'https://goldmansachs.tal.net/vx/lang-en-GB/mobile-0/brand-2/xf-caeaaeb33c61/candidate/so/pm/1/pl/1/opp/2-Summer-Analyst-Summer-Associate-Internship-programs/en-GB',
     ),
+    JobListing(
+        company: 'IBM',
+        batch: 'Not specified',
+        role: "Software Developer Intern",
+        salary: "Not specified",
+        link:
+            "https://careers.ibm.com/job/19976272/software-developer-intern-bangalore-in/"),
+    JobListing(
+        company: 'IMC',
+        batch: 'B.tech 2024 graduates',
+        role: "Graduate Software Engineer",
+        salary: "Not specified",
+        link:
+            "https://careers.imc.com/us/en/job/4282981101/Graduate-Software-Engineer"),
+    JobListing(
+        company: 'ZF Group',
+        batch: 'UG in Mechanical or Mechatronics domain',
+        role: "Graduate Engineer Trainee (GET)",
+        salary: "Not specified",
+        link:
+            "https://jobs.zf.com/job/Chennai-GET-B_TechB_E-Mechanical-Mechatronics-TN-631604/1018168301/"),
   ];
 
   @override
